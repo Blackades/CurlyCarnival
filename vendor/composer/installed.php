fc1c8a56088c7b32f1106d3f491aa2617366d79d
Blackades <blacktori767@gmail.com>
2025-12-02 20:25:44 +0000

    Add payment gateway plugins

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hotspotbilling/phpnuxbill',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd5bf6f05dc19cbd6e47d42ccf8ac0d797db5baae',
+        'type' => 'template',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hotspotbilling/phpnuxbill' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd5bf6f05dc19cbd6e47d42ccf8ac0d797db5baae',
+            'type' => 'template',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
